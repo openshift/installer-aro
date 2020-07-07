9b0bf599641f890609db6827aeca4b61a57e6e1b
Leszek Jakubowski <ljakubow@redhat.com>
2020-07-07 21:43:45 +0200

    CARRY: HACK: add bootstrap node MDSD logging

--- pkg/asset/ignition/bootstrap/common.go
+++ pkg/asset/ignition/bootstrap/common.go
@@ -26,6 +26,7 @@ import (
 
 	"github.com/openshift/installer/data"
 	"github.com/openshift/installer/pkg/asset"
+	"github.com/openshift/installer/pkg/asset/bootstraplogging"
 	"github.com/openshift/installer/pkg/asset/ignition"
 	"github.com/openshift/installer/pkg/asset/ignition/bootstrap/baremetal"
 	"github.com/openshift/installer/pkg/asset/ignition/bootstrap/vsphere"
@@ -58,6 +59,8 @@ var (
 		"coredns.service",
 		"ironic.service",
 		"master-bmh-update.service",
+		"fluentbit.service",
+		"mdsd.service",
 	}
 )
 
@@ -79,6 +82,7 @@ type bootstrapTemplateData struct {
 	UseIPv6ForNodeIP      bool
 	IsOKD                 bool
 	BootstrapNodeIP       string
+	LoggingConfig         *bootstraplogging.Config
 }
 
 // platformTemplateData is the data to use to replace values in bootstrap
@@ -99,6 +103,7 @@ func (a *Common) Dependencies() []asset.Asset {
 	return []asset.Asset{
 		&baremetal.IronicCreds{},
 		&installconfig.InstallConfig{},
+		&bootstraplogging.Config{},
 		&kubeconfig.AdminInternalClient{},
 		&kubeconfig.Kubelet{},
 		&kubeconfig.LoopbackClient{},
@@ -227,12 +232,13 @@ func (a *Common) Files() []*asset.File {
 // getTemplateData returns the data to use to execute bootstrap templates.
 func (a *Common) getTemplateData(dependencies asset.Parents, bootstrapInPlace bool) *bootstrapTemplateData {
 	installConfig := &installconfig.InstallConfig{}
+	loggingConfig := &bootstraplogging.Config{}
 	proxy := &manifests.Proxy{}
 	releaseImage := &releaseimage.Image{}
 	rhcosImage := new(rhcos.Image)
 	bootstrapSSHKeyPair := &tls.BootstrapSSHKeyPair{}
 	ironicCreds := &baremetal.IronicCreds{}
-	dependencies.Get(installConfig, proxy, releaseImage, rhcosImage, bootstrapSSHKeyPair, ironicCreds)
+	dependencies.Get(installConfig, proxy, releaseImage, rhcosImage, bootstrapSSHKeyPair, ironicCreds, loggingConfig)
 
 	etcdEndpoints := make([]string, *installConfig.Config.ControlPlane.Replicas)
 
@@ -300,6 +306,7 @@ func (a *Common) getTemplateData(dependencies asset.Parents, bootstrapInPlace bo
 		Registries:            registries,
 		BootImage:             string(*rhcosImage),
 		PlatformData:          platformData,
+		LoggingConfig:         loggingConfig,
 		ClusterProfile:        clusterProfile,
 		BootstrapInPlace:      bootstrapInPlaceConfig,
 		UseIPv6ForNodeIP:      APIIntVIPonIPv6,
