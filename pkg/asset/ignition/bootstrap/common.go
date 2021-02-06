c3a77a1a74359b7134d7a73b2d682343fff539b0
Jim Minter <jminter@redhat.com>
2021-02-06 10:31:06 -0600

    HACK: set gateway domains in dnsmasq config
    This is required for egress lockdown support.

--- pkg/asset/ignition/bootstrap/common.go
+++ pkg/asset/ignition/bootstrap/common.go
@@ -210,7 +210,7 @@ func (a *Common) generateConfig(dependencies asset.Parents, templateData *bootst
 		}},
 	)
 
-	dnsmasqIgnConfig, err := dnsmasq.Ignition3Config(installConfig.Config.ClusterDomain(), aroDNSConfig.APIIntIP, aroDNSConfig.IngressIP)
+	dnsmasqIgnConfig, err := dnsmasq.Ignition3Config(installConfig.Config.ClusterDomain(), aroDNSConfig.APIIntIP, aroDNSConfig.IngressIP, aroDNSConfig.GatewayDomains, aroDNSConfig.GatewayPrivateEndpointIP)
 	if err != nil {
 		return err
 	}
