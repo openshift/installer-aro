60159c0100cf5ec866632139d6d1162f2703f694
Jim Minter <jminter@redhat.com>
2021-02-06 07:52:47 -0600

    HACK: make ingress IP pre-configurable

--- pkg/asset/manifests/operators.go
+++ pkg/asset/manifests/operators.go
@@ -74,6 +74,8 @@ func (m *Manifests) Dependencies() []asset.Asset {
 		&bootkube.MachineConfigServerTLSSecret{},
 		&bootkube.OpenshiftConfigSecretPullSecret{},
 		&bootkube.AROWorkerRegistries{},
+		&bootkube.AROIngressService{},
+		&bootkube.ARODNSConfig{},
 	}
 }
 
@@ -140,11 +142,13 @@ func (m *Manifests) generateBootKubeManifests(dependencies asset.Parents) []*ass
 	installConfig := &installconfig.InstallConfig{}
 	mcsCertKey := &tls.MCSCertKey{}
 	rootCA := &tls.RootCA{}
+	aroDNSConfig := &bootkube.ARODNSConfig{}
 	dependencies.Get(
 		clusterID,
 		installConfig,
 		mcsCertKey,
 		rootCA,
+		aroDNSConfig,
 	)
 
 	templateData := &bootkubeTemplateData{
@@ -158,6 +162,8 @@ func (m *Manifests) generateBootKubeManifests(dependencies asset.Parents) []*ass
 		IsSCOS:              installConfig.Config.IsSCOS(),
 		IsOKD:               installConfig.Config.IsOKD(),
 		AROWorkerRegistries: aroWorkerRegistries(installConfig.Config.ImageContentSources),
+		AROIngressIP:        aroDNSConfig.IngressIP,
+		AROIngressInternal:  installConfig.Config.Publish == types.InternalPublishingStrategy,
 	}
 
 	files := []*asset.File{}
@@ -168,6 +174,7 @@ func (m *Manifests) generateBootKubeManifests(dependencies asset.Parents) []*ass
 		&bootkube.MachineConfigServerTLSSecret{},
 		&bootkube.OpenshiftConfigSecretPullSecret{},
 		&bootkube.AROWorkerRegistries{},
+		&bootkube.AROIngressService{},
 	} {
 		dependencies.Get(a)
 		for _, f := range a.Files() {
