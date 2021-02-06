3dd83ef98764922827107ab8badc4dc85f69ad18
Jim Minter <jminter@redhat.com>
2021-02-06 10:56:41 -0600

    HACK: update MCS to use IP on ARO

--- pkg/asset/ignition/machine/worker_ignition_customizations_test.go
+++ pkg/asset/ignition/machine/worker_ignition_customizations_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/openshift/installer/pkg/asset"
 	"github.com/openshift/installer/pkg/asset/ignition"
 	"github.com/openshift/installer/pkg/asset/installconfig"
+	"github.com/openshift/installer/pkg/asset/templates/content/bootkube"
 	"github.com/openshift/installer/pkg/asset/tls"
 	"github.com/openshift/installer/pkg/ipnet"
 	"github.com/openshift/installer/pkg/types"
@@ -51,8 +52,12 @@ func TestWorkerIgnitionCustomizationsGenerate(t *testing.T) {
 			err := rootCA.Generate(nil)
 			assert.NoError(t, err, "unexpected error generating root CA")
 
+			aroDNSConfig := &bootkube.ARODNSConfig{}
+			err = aroDNSConfig.Generate(nil)
+			assert.NoError(t, err, "unexpected error generating ARO DNS Config")
+
 			parents := asset.Parents{}
-			parents.Add(installConfig, rootCA)
+			parents.Add(installConfig, rootCA, aroDNSConfig)
 
 			worker := &Worker{}
 			err = worker.Generate(parents)
